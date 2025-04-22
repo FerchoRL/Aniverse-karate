e15f60a1c21da9fe4e53748d3f2742d00618cc82
Fernando RL <rlfernando7@gmail.com>
2025-04-22 01:56:03 -0600

    feat: add feature for successful user registration with USER_ROLE and ADMIN_ROLE

--- src/test/java/AniverseApp/helpers/DataGenerator.java
+++ src/test/java/AniverseApp/helpers/DataGenerator.java
@@ -1,5 +1,17 @@
 package AniverseApp.helpers;
+import com.github.javafaker.Faker;
 
 public class DataGenerator {
+    private static Faker faker = new Faker();
+
+    public static String getRandomEmail(){
+        String email = faker.name().firstName().toLowerCase() + faker.random().nextInt(0,100) + "@test.com";
+        return email;
+    }
+
+    public static String getRandomUserName(){
+        String user = faker.name().username();
+        return user;
+    }
     
 }
